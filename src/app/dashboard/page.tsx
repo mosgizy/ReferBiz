5da776ac65a0b12c765ba98f03e9dcb6f94f63c8
mosgizy <moshood521@gmail.com>
2023-08-24 13:19:08 +0100

    fixed dashboard desktop design

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -63,7 +63,7 @@ const Page = () => {
 
   return (
     <ProtectedRoute>
-      <section className="section text-left py-4 mb-20 relative md:max-w-[47rem] md:mx-auto">
+      <section className="section text-left py-4 mb-20 relative">
         <div className="flex-center justify-between">
           <Link
             href="/"
@@ -90,119 +90,97 @@ const Page = () => {
             )}
           </div>
         </div>
-        <div className="my-4 flex-center justify-between">
-          <div className="flex flex-col gap-2">
-            <div className="font-semibold text-2xl">₦ {amount}</div>
-            <p className="text-xs text-text-color">Wallet balance</p>
-          </div>
-          <div className="flex flex-col gap-2">
-            <div className="font-semibold text-2xl">{referralCount}</div>
-            <p className="text-xs text-text-color">Referals</p>
-          </div>
-          <div className="flex flex-col gap-2">
-            <div className="font-semibold text-2xl">{linkGenereatedCount}</div>
-            <p className="text-xs text-text-color">Links generated</p>
-          </div>
-        </div>
-        <div className="mt-10 flex flex-col gap-3">
-          <div className="px-5 py-4 shadow-google">
-            <div className="flex justify-between gap-8">
-              <div className="flex flex-col gap-1">
-                <h1 className="text-base text-header">
-                  Share your generated campaign link
-                </h1>
-                <p className="text-text-color text-xs">
-                  This campaign link is what you give referrer to start
-                  referring.
-                </p>
+        <div className="md:max-w-[47rem] md:mx-auto md:mt-14">
+          <div className="my-4 flex-center justify-between">
+            <div className="flex flex-col gap-2">
+              <div className="font-semibold text-2xl">₦ {amount}</div>
+              <p className="text-xs text-text-color">Wallet balance</p>
+            </div>
+            <div className="flex flex-col gap-2">
+              <div className="font-semibold text-2xl">{referralCount}</div>
+              <p className="text-xs text-text-color">Referals</p>
+            </div>
+            <div className="flex flex-col gap-2">
+              <div className="font-semibold text-2xl">
+                {linkGenereatedCount}
               </div>
-              <Image
+              <p className="text-xs text-text-color">Links generated</p>
+            </div>
+          </div>
+          <div className="mt-10 flex flex-col gap-3">
+            <div className="px-5 py-4 shadow-google">
+              <div className="flex justify-between gap-8">
+                <div className="flex flex-col gap-1">
+                  <h1 className="text-base text-header">
+                    Share your generated campaign link
+                  </h1>
+                  <p className="text-text-color text-xs">
+                    This campaign link is what you give referrer to start
+                    referring.
+                  </p>
+                </div>
+                <Image
+                  onClick={() => handleCopyToClipboard(linkGenereated)}
+                  src={compShare}
+                  alt=""
+                  width={64}
+                  height={64}
+                />
+              </div>
+              <button
                 onClick={() => handleCopyToClipboard(linkGenereated)}
-                src={compShare}
-                alt=""
-                width={64}
-                height={64}
-              />
+                className="btn bg-green-500 flex-center justify-center gap-1 text-white w-full mt-4"
+              >
+                <FaCopy /> COPY LINK
+              </button>
             </div>
-            <button
-              onClick={() => handleCopyToClipboard(linkGenereated)}
-              className="btn bg-green-500 flex-center justify-center gap-1 text-white w-full mt-4"
-            >
-              <FaCopy /> COPY LINK
-            </button>
           </div>
-          {/* <div className="px-5 py-4 shadow-google">
-            <div className="flex justify-between gap-8">
-              <div className="flex flex-col gap-1">
-                <h1 className="text-base text-header">
-                  Share your generated payment link
-                </h1>
-                <p className="text-text-color text-xs">
-                  Share this link to customers when they want to pay through
-                  referral.
-                </p>
+          <div className="mt-5">
+            <div className="flex-center justify-between">
+              <div className="text-base font-semibold text-header">
+                Referral Activity
               </div>
-              <Image
-                onClick={() => handleCopyToClipboard(paystack)}
-                src={paymentShare}
-                alt=""
-                width={64}
-                height={64}
-              />
+              <div className="text-primary-btn text-xs font-medium cursor-pointer">
+                See all
+              </div>
             </div>
-            <button
-              onClick={() => handleCopyToClipboard(paystack)}
-              className="btn flex-center justify-center gap-1 text-white w-full mt-4"
-            >
-              <FaCopy /> COPY LINK
-            </button>
-          </div> */}
-        </div>
-        <div className="mt-5">
-          <div className="flex-center justify-between">
-            <div className="text-base font-semibold text-header">
-              Referral Activity
-            </div>
-            <div className="text-primary-btn text-xs font-medium cursor-pointer">
-              See all
-            </div>
-          </div>
-          <div>
-            {activity ? (
-              <div className="mt-4 flex flex-col gap-4">
-                <div className="flex-center justify-between">
-                  <div className="flex-center gap-3">
-                    <Image src={noteImage} alt="" height={48} width={48} />
-                    <div className="flex flex-col gap-1">
-                      <h1 className="text-header text-sm font-medium">
-                        Ayo paid using Daniel...
-                      </h1>
-                      <p className="text-text-color text-xs">
-                        Daniel . 8:30 PM
-                      </p>
+            <div>
+              {activity ? (
+                <div className="mt-4 flex flex-col gap-4">
+                  <div className="flex-center justify-between">
+                    <div className="flex-center gap-3">
+                      <Image src={noteImage} alt="" height={48} width={48} />
+                      <div className="flex flex-col gap-1">
+                        <h1 className="text-header text-sm font-medium">
+                          Ayo paid using Daniel...
+                        </h1>
+                        <p className="text-text-color text-xs">
+                          Daniel . 8:30 PM
+                        </p>
+                      </div>
                     </div>
+                    <div>₦20,000.00</div>
                   </div>
-                  <div>₦20,000.00</div>
-                </div>
-                <div className="flex-center justify-between">
-                  <div className="flex-center gap-3">
-                    <Image src={linkImage} alt="" height={48} width={48} />
-                    <div className="flex flex-col gap-1">
-                      <h1 className="text-header text-sm font-medium">
-                        Referal link generated
-                      </h1>
-                      <p className="text-text-color text-xs">
-                        Daniel . 8:30 PM
-                      </p>
+                  <div className="flex-center justify-between">
+                    <div className="flex-center gap-3">
+                      <Image src={linkImage} alt="" height={48} width={48} />
+                      <div className="flex flex-col gap-1">
+                        <h1 className="text-header text-sm font-medium">
+                          Referal link generated
+                        </h1>
+                        <p className="text-text-color text-xs">
+                          Daniel . 8:30 PM
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ) : (
-              <div className="mt mt-12 flex justify-center">
-                <Image src={activityImage} alt="" height={100} width={100} />
-              </div>
-            )}
+              ) : (
+                <div className="mt mt-12 flex justify-center">
+                  <Image src={activityImage} alt="" height={100} width={100} />
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </section>
