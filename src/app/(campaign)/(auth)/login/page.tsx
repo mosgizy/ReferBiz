2745d3b886ec11f5f336dbb0749a37aac4eab819
mosgizy <moshood521@gmail.com>
2023-08-24 12:59:26 +0100

    added register and login desktop

--- src/app/(campaign)/(auth)/login/page.tsx
+++ src/app/(campaign)/(auth)/login/page.tsx
@@ -45,33 +45,43 @@ const Page = () => {
   }, [session, router]);
 
   return (
-    <>
-      <div className="flex-center justify-center mt-12">
-        <Image src={loginImage} alt="" height={500} width={250} priority />
+    <div className="md:flex-center md:gap-8 md:section">
+      <div className="flex-center justify-center flex-[1_1_50%] md:bg-[#F3F9ED] md:rounded-[3rem] md:h-[40rem] md:w-[32rem] md:py-24 md:px-20">
+        <Image
+          src={loginImage}
+          alt=""
+          height={800}
+          width={450}
+          priority
+          className="h-full"
+        />
       </div>
-      <div className="mt-16">
-        <h1 className="font-semibold text-3xl text-header">Welcome back</h1>
-        <p className="text-text-color text-sm mt-2">
-          Let us resume from where we stopped at
-        </p>
-      </div>
-      <div className="[&>p]:text-text-color sticky bottom-0 [&>p]:text-xs mt-8 flex flex-col gap-6">
-        <div
-          onClick={handleLogin}
-          className="text-header text-sm flex-center justify-center gap-3 shadow-google rounded-full border border-light-gray px-4 py-3 cursor-pointer"
-        >
-          <Image src={googleIcon} alt="" height={18} width={18} />
-          <span>Continue with google</span>
+      <div className="flex flex-col gap-12 flex-[1_1_45%]">
+        <div className="mt-16 flex flex-col">
+          <h1 className="font-semibold text-3xl md:text-4xl text-header">
+            Welcome back
+          </h1>
+          <p className="text-text-color text-sm mt-2">
+            Let us resume from where we stopped at
+          </p>
         </div>
-        {/* <SignInButton /> */}
-        <p>
-          Don’t have an account?{' '}
-          <Link href={'/register'} className="text-log cursor-pointer">
-            Sign in
-          </Link>
-        </p>
+        <div className="[&>p]:text-text-color sticky bottom-0 [&>p]:text-xs mt-8 flex flex-col gap-6">
+          <div
+            onClick={handleLogin}
+            className="text-header text-sm flex-center justify-center gap-3 shadow-google rounded-full border border-light-gray px-4 py-3 cursor-pointer"
+          >
+            <Image src={googleIcon} alt="" height={18} width={18} />
+            <span>Continue with google</span>
+          </div>
+          <p>
+            Don’t have an account?{' '}
+            <Link href={'/register'} className="text-log cursor-pointer">
+              Sign in
+            </Link>
+          </p>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/app/(campaign)/(auth)/register/page.tsx
+++ src/app/(campaign)/(auth)/register/page.tsx
@@ -11,13 +11,20 @@ const Page = () => {
   sendData();
 
   return (
-    <div className="md:flex-center">
-      <div className="flex-center justify-center flex-">
-        <Image src={registerImage} alt="" height={500} width={250} priority />
+    <div className="md:flex-center md:gap-8 md:section">
+      <div className="flex-center justify-center flex-[1_1_50%] md:bg-[#F3F9ED] md:rounded-[3rem] md:h-[40rem] md:w-[32rem] md:py-24 md:px-20">
+        <Image
+          src={registerImage}
+          alt=""
+          height={800}
+          width={250}
+          priority
+          className="h-full"
+        />
       </div>
-      <div className="flex flex-col gap-12">
+      <div className="flex flex-col gap-12 flex-[1_1_45%]">
         <div className="mt-16 flex flex-col">
-          <h1 className="font-semibold text-3xl text-header">
+          <h1 className="font-semibold text-3xl md:text-4xl text-header">
             Create your account
           </h1>
           <p className="text-text-color text-sm mt-2">
